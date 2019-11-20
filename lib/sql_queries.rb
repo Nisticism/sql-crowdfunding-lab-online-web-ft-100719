9805cca1a9578e5f60699ba399fe44586dfc93e1
Hans Foster <fosterhans@gmail.com>
2019-11-20 23:36:57 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT title, (pledges.amount - funding_goal) FROM projects JOIN pledges on pledges.project_id = projects.id HAVING (pledges.amount - funding_goal > 0)"
+  "SELECT title, (pledges.amount - funding_goal) FROM projects JOIN pledges on pledges.project_id = projects.id GROUP BY titles HAVING (pledges.amount - funding_goal > 0)"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
