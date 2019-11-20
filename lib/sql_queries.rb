f214ee4659706b9df3d0f215680370f5b85fce53
Hans Foster <fosterhans@gmail.com>
2019-11-20 23:39:03 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,11 +15,11 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT title, (pledges.amount - funding_goal) FROM projects JOIN pledges on pledges.project_id = projects.id GROUP BY titles HAVING (pledges.amount - funding_goal > 0)"
+  "SELECT title, (pledges.amount - funding_goal) FROM projects JOIN pledges on pledges.project_id = projects.id GROUP BY title HAVING (pledges.amount - funding_goal > 0)"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "SELECT name, pledges.amount FROM users JOIN pledges ON pledges.user_id = users.id GROUP BY users.name ORDER BY SUM(pledges.amount)"
+  "SELECT name, SUM(pledges.amount) FROM users JOIN pledges ON pledges.user_id = users.id GROUP BY users.name ORDER BY SUM(pledges.amount)"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
